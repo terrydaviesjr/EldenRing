cf369f70a7d9356caeec68a1ffa1ce373655227b
Terry Davies <terrydaviesjr@gmail.com>
2022-03-31 17:44:01 -0400

    Null safe alias compare function. Added in some more armament option values

--- angular/src/app/common/domain/function/alias-compare.function.ts
+++ angular/src/app/common/domain/function/alias-compare.function.ts
@@ -1,3 +1,7 @@
 export function aliasCompareFn(o1: {alias: string}, o2: {alias: string}): boolean {
-    return o1.alias === o2.alias;
+    if (o1 && o2) {
+        return o1.alias === o2.alias;
+    } else {
+        return false;
+    }
 }
